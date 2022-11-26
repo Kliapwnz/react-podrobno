0138a1fb3f8b44a9e3b84205737cfa66fb05de49
Evgeniy Klimovch <kliasanik@gmail.com>
2022-11-26 17:15:08 +0300

    add props for Pagetitle

--- src/App.tsx
+++ src/App.tsx
@@ -3,22 +3,33 @@ import './App.css';
 import {Accordion} from "./Components/Accordion/Accordion";
 import {Rating} from "./Components/Rating/Rating";
 
+type pageTitlePropsType={
+    title:string
+}
+
+
 function App() {
     console.log("App rendered")
     return (
         <div>
-            <AppTitle/>
+            <PageTitle title={"This is APP component"}/>
+            <PageTitle title={"Manchester City"}/>
             Article1
             <Rating value={2}/>
             <Accordion/>
             Article2
+            <Rating value={0}/>
+            <Rating value={1}/>
+            <Rating value={2}/>
             <Rating value={3}/>
+            <Rating value={4}/>
+            <Rating value={5}/>
         </div>
     );
 }
 
-function AppTitle() {
-    return <div>This is APP component</div>
+function PageTitle(props:pageTitlePropsType) {
+    return <div>{props.title}</div>
 }
 
 
