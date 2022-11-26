931281aa5dd1922db4174698b422008b6735631f
Evgeniy Klimovch <kliasanik@gmail.com>
2022-11-26 14:36:24 +0300

    add Rating and Accordion

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,33 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      Hello
+    <div>
+      This is APP component
     </div>
   );
 }
 
+function Rating() {
+    return(
+        <div>
+        <div>star</div>
+        <div>star</div>
+        <div>star</div>
+        <div>star</div>
+        <div>star</div>
+        </div>
+    )
+}
+function Accordion() {
+    return(
+        <div>
+            <h3>Menu</h3>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+        </div>
+    )
+}
 export default App;
