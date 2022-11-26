98b434548f4f5963545b16a0d2eb3f1904021255
Evgeniy Klimovch <kliasanik@gmail.com>
2022-11-26 15:23:00 +0300

    Create Accordion.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import {Accordion} from "./Components/Accordion";
 
 function App() {
     console.log("App rendered")
@@ -30,15 +31,7 @@ function Rating() {
     )
 }
 
-function Accordion() {
-    console.log("Accordion rendered")
-    return (
-        <div>
-           <AccordionTitle/>
-           <AccordionBody/>
-        </div>
-    )
-}
+
 
 function Star() {
     console.log("Star rendered")
@@ -47,22 +40,7 @@ function Star() {
     )
 }
 
-function AccordionTitle() {
-    console.log("AccordionTitle rendering")
-    return(
-        <h3>Menu</h3>
-    )
-}
-function AccordionBody() {
-    console.log("AccordionBody rendering")
-    return (<ul>
-            <li>1</li>
-            <li>2</li>
-            <li>3</li>
-        </ul>
 
-    )
-}
 
 
 export default App;
