2445ddc7214cbe7f70351dba4a3cbbee01f0079b
Evgeniy Klimovch <kliasanik@gmail.com>
2022-11-27 12:25:35 +0300

    add styles

--- src/Components/OnOff/OnOff.tsx
+++ src/Components/OnOff/OnOff.tsx
@@ -1,9 +1,21 @@
 import React from 'react';
 
-export const OnOff = () => {
+type PropsType = {
+    on: boolean
+}
+
+export const OnOff = (props: PropsType) => {
+   const onStyle ={};
+   const offStyle ={};
+   const indicatorStyle ={};
+
+
+
     return (
         <div>
-
+            <div>1</div>
+            <div>2</div>
+            <div>3</div>
         </div>
     );
 };
