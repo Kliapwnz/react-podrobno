25760b5277acf70c9b9d4819467259a80543d152
Evgeniy Klimovch <kliasanik@gmail.com>
2022-11-27 12:37:45 +0300

    padding

--- src/Components/OnOff/OnOff.tsx
+++ src/Components/OnOff/OnOff.tsx
@@ -9,13 +9,16 @@ export const OnOff = (props: PropsType) => {
        width: "30px",
        height: "20px",
        border: "1px solid black",
-       display: "inline-block"
+       display: "inline-block",
+       padding: "2px"
    };
    const offStyle ={
        width: "30px",
        height: "20px",
        border: "1px solid black",
-       display: "inline-block"
+       display: "inline-block",
+       marginLeft: "2px",
+       padding: "2px"
    };
    const indicatorStyle ={
        width: "10px",
