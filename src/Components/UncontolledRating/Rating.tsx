9d4b68c862d18614325429d94a5a5deedce7933d
Evgeniy Klimovch <kliasanik@gmail.com>
2022-11-27 15:17:46 +0300

    add buttons

--- src/Components/UncontolledRating/Rating.tsx
+++ src/Components/UncontolledRating/Rating.tsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 type starPropsType = {
     selected: boolean
-
 }
+
+
 type ratingPropsType = {
 
 }
@@ -11,14 +12,14 @@ type ratingPropsType = {
 export const UncontrolledRating = (props: ratingPropsType) => {
     console.log("Rating rendered")
 
-
+let [value, setValue] = useState(3)
     return (
         <div>
-            <Star selected={props.value > 0}/>
-            <Star selected={props.value > 1}/>
-            <Star selected={props.value > 2}/>
-            <Star selected={props.value > 3}/>
-            <Star selected={props.value > 4}/>
+            <Star selected={value > 0}/><button onClick={()=>{setValue(1)}}>1</button>
+            <Star selected={value > 1}/><button onClick={()=>{setValue(2)}}>2</button>
+            <Star selected={value > 2}/><button onClick={()=>{setValue(3)}}>3</button>
+            <Star selected={value > 3}/><button onClick={()=>{setValue(4)}}>4</button>
+            <Star selected={value > 4}/><button onClick={()=>{setValue(5)}}>5</button>
 
         </div>
     )
